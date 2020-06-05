864e5ecd0966762884bb5134dda8246a150c7eb9
RAMCHARANTEJA <RAMCHARANTEJA@Ramchran>
2020-06-05 16:26:16 +0530

    Model Class Name Milk is created

---
+++ RCMC/src/main/java/com/app/model/Milk.java
@@ -0,0 +1,13 @@
+package com.app.model;
+
+import lombok.Data;
+
+@Data
+public class Milk {
+	private Integer id;
+	private Integer milkNo;
+	private String milkName;
+	private String milkAdress;
+	private String milkEmail;
+
+}
