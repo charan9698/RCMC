091417f0c8b0128308b6962689fd57d30f29dc23
RAMCHARANTEJA <RAMCHARANTEJA@Ramchran>
2020-06-05 16:58:22 +0530

    pending work done

--- RCMC/src/main/java/com/app/service/impl/MilkServiceImpl.java
+++ RCMC/src/main/java/com/app/service/impl/MilkServiceImpl.java
@@ -1,5 +1,58 @@
 package com.app.service.impl;
 
-public class MilkServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.model.Milk;
+import com.app.repo.MilkRepository;
+import com.app.service.IMilkService;
+
+@Service
+public class MilkServiceImpl implements IMilkService {
+	@Autowired
+	private MilkRepository repo;
+
+
+	public Integer saveMilk(Milk m) {
+		Integer id=repo.save(m).getId();
+
+		return id;
+	}
+
+	public void updateMilk(Milk m) {
+		repo.save(m);
+
+	}
+
+	public void deleteMilk(Integer id) {
+		repo.deleteById(id);		
+	}
+
+	public Milk getOneMilk(Integer id) {
+		Optional<Milk> opt=repo.findById(id);
+		if (opt.isPresent()) {
+			return opt.get();
+
+		}
+
+		return null;
+	}
+
+	public List<Milk> getAllMilks() {
+		List<Milk> list=repo.findAll();
+		return list;
+	}
+
+	public boolean isMilkExist(Integer id) {
+		boolean exist=repo.existsById(id);
+		return exist;
+	}
+
+
+
+
 
 }
